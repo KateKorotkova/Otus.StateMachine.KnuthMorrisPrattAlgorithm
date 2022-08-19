43cb5b71237074c7871f0d4f553b506e18f8dbe0
Ekaterina Korotkova <e.korotkova.2013@yandex.ru>
2022-08-19 21:43:08 +0300

    initial commit

---
+++ Otus.StateMachine.KnuthMorrisPrattAlgorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Otus.StateMachine.KnuthMorrisPrattAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
